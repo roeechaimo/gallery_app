1a2e5ebc551341f33ee045d21888bbc924c1ff79
roeechaimo <roee84@gmail.com>
2019-10-08 13:35:10 +0300

    add sorting functionality

--- gallery-app/src/app/my-gallery/my-gallery.component.ts
+++ gallery-app/src/app/my-gallery/my-gallery.component.ts
@@ -18,6 +18,7 @@ export class MyGalleryComponent implements OnInit {
   public images: Image[];
   public filteredImages: Image[];
   public searchValue: string = "";
+  public sortValue: string = "";
 
   constructor(private _feedService: FeedService) {}
 
@@ -27,8 +28,9 @@ export class MyGalleryComponent implements OnInit {
 
   public getFeed() {
     this._feedService.getFeed(this.feed).subscribe((data: Image[]) => {
-      this.images = data;
-      this.filteredImages = data;
+      const images = data;
+      this.images = [...images];
+      this.filteredImages = [...images];
     });
   }
 
@@ -36,5 +38,40 @@ export class MyGalleryComponent implements OnInit {
     this.filteredImages = this.images.filter(image =>
       image.title.includes(this.searchValue)
     );
+  }
+
+  public onSortChange(value: string) {
+    const images = [...this.images];
+
+    if (value === "title") {
+      return this.filteredImages.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+
+        return 0;
+      });
+    }
+
+    if (value === "date") {
+      return this.filteredImages.sort((a, b) => {
+        const bDate = new Date(b.date);
+        const aDate = new Date(a.date);
+
+        if (aDate < bDate) {
+          return -1;
+        }
+        if (aDate > bDate) {
+          return 1;
+        }
+
+        return 0;
+      });
+    }
+
+    this.filteredImages = [...images];
   }
 }
