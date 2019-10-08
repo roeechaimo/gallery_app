c6cc8c3c51ab7d4d9d99c71c188e49ed85cee623
roeechaimo <roee84@gmail.com>
2019-10-08 11:32:29 +0300

    present images

--- gallery-app/src/app/my-gallery/components/image/image.component.ts
+++ gallery-app/src/app/my-gallery/components/image/image.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-image',
-  templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  selector: "app-image",
+  templateUrl: "./image.component.html",
+  styleUrls: ["./image.component.scss"]
 })
 export class ImageComponent implements OnInit {
+  @Input() title: string;
+  @Input() url: string;
+  @Input() date: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(`${this.title},  ${this.url},  ${this.date}`);
   }
-
 }
